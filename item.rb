0e282014f9efda1cc6c5e12de3ff129bfc002b28
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-07 22:55:29 +0300

    Create can_be_archived method

--- item.rb
+++ item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
     attribute_accesor :genre, :author, :label
     attribute_reader :publish_date
@@ -11,6 +13,11 @@ class Item
         @label = nil
     end
     
-
-
+    def can_be_archived?
+        now = Date.today
+        before = Date.parse(publish_date.to_s)
+        difference = (now - before).to_i
+        difference_year = difference / 365
+        difference_year < 10
+    end
 end
