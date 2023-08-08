ec2fa30e1984fe9e5f17b19b19404e483bd0b463
Pablo <ezekielgonzalezr@gmail.com>
2023-08-07 21:08:57 -0600

    Add genre class

---
+++ genre.rb
@@ -0,0 +1,15 @@
+class Genre
+    attr_accessor :name
+    attr_reader :id, :items
+
+    def initialize(name)
+      @id = rand(0..1000) 
+      @name = name
+      @items = []
+    end
+
+    def add_item(item)
+        @items << item
+        item.genre = self unless item.genre
+    end
+end
